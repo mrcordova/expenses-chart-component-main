447d901cac1d9cbc44b7190c864edeedb369c58e
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-06-17 16:35:22 -0700

    add logic for current day

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 import data from "../data.json" assert { type: "json" };
+
+const dayNames = {
+  1: "mon",
+  2: "tue",
+  3: "wed",
+  4: "thu",
+  5: "fri",
+  6: "sat",
+  7: "sun",
+};
+window.addEventListener("load", () => {
+  const barContainerDivs = document.querySelectorAll(".bar-container");
+  const highestAmount = data.reduce((currVal, nextVal) => {
+    if (currVal.amount > nextVal.amount) {
+      return currVal;
+    }
+    return nextVal;
+  });
+
+  const currentDay = dayNames[new Date().getDay()];
+  const currentDayBar = document.getElementById(`${currentDay}`);
+  currentDayBar.previousElementSibling.classList.add("bar-active");
+  //   console.log(currentDayBar.previousElementSibling);
+  for (const barContainerDiv of barContainerDivs) {
+    barContainerDiv;
+  }
+});
 console.log(data);
